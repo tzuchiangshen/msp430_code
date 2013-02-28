7384229fcf1d36cc1ec9430b26da7b2dc43ae469
RickKimball <rick@kimballsoftware.com>
2013-02-28 18:04:54 -0500

    Add minimal files to test fabooh as a build target in Energia.

---
+++ fabooh/examples/Fabooh/blink/blink.ino
@@ -0,0 +1,42 @@
+/**
+ * blink.ino - blink test
+ *
+ * Pins Used: RED_LED, GREEN_LED, PUSH2
+ *
+ */
+
+#define SMALL_INIT4 /* don't initialize .bss and .data sections */
+#include <main.h>
+
+using namespace GPIO;
+
+void setup() {
+
+  RED_LED::pinMode(OUTPUT);
+  GREEN_LED::pinMode(OUTPUT);
+  PUSH2::setmode_inputpullup();
+
+  GREEN_LED::low();
+  RED_LED::high();
+
+}
+
+void loop() {
+  
+  // block loop if user holds down the button
+  if ( !PUSH2::read() ) {
+    do {
+      delay_msecs(10); // debounce switch
+    } while(!PUSH2::read());
+  }
+
+  if ( 1 /* same port do both at the same time*/) {
+    RED_LED::toggle_pins(RED_LED::pin_mask|GREEN_LED::pin_mask);
+  }
+  else {
+    RED_LED::toggle();
+    GREEN_LED::toggle();
+  }
+  delay_msecs(500);
+}
+
