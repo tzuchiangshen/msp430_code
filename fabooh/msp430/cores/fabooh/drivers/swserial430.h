38fe9c57e4c868c840fd98e04ab534bda7b92eb1
RickKimball <rick@kimballsoftware.com>
2013-03-04 15:38:59 -0500

    fix inline asm constraints

--- fabooh/msp430/cores/fabooh/drivers/swserial430.h
+++ fabooh/msp430/cores/fabooh/drivers/swserial430.h
@@ -3,8 +3,8 @@
  *
  * Created: Nov-12-2012
  *  Author: rick@kimballsoftware.com
- *    Date: 03-02-2013
- * Version: 1.0.3
+ *    Date: 03-04-2013
+ * Version: 1.0.4
  *
  * Acknowledgments:
  *  Inspiration for cycle counting RX/TX routines from Kevin Timmerman.
@@ -102,44 +102,45 @@ struct serial_base_sw_t {
 /*
  * serial_base_sw_t<>::_write() - write one byte to the TX pin
  */
-template <uint32_t BAUD, uint32_t MCLK_HZ, typename TXPIN, typename RXPIN>
-void serial_base_sw_t<BAUD, MCLK_HZ, TXPIN, RXPIN>::_write(unsigned c)
-{
-    __asm__ (
-            "   bis     #0x0300, %[c]        ; Add Stop bit(s) to tx char\n"
-            "   mov     %[bit_dur], r14      ; Bit duration\n"
-            "   mov     %[tx_bit_mask], r12  ; Serial output bitmask\n"
+template<uint32_t BAUD, uint32_t MCLK_HZ, typename TXPIN, typename RXPIN>
+void serial_base_sw_t<BAUD, MCLK_HZ, TXPIN, RXPIN>::_write(register unsigned c) {
+    register unsigned bit_dur = bit_rate.dur;
+    register unsigned mask = TXPIN::pin_mask;
+    register unsigned cnt = 0;
+
+    c |= 0x300; // add stop bits
+
+    __asm__ volatile (
             "   jmp     3f                   ; Send start bit...\n"
-            "1:\n"
-            "   mov     r14, r13             ; Get bit duration\n"
-            "2:\n"
+            "1:                              ;\n"
+            "   mov     %[bit_dur],%[cnt]    ; Get bit duration\n"
+            "2:                              ;\n"
             "   nop                          ; 4 cycle loop\n"
-            "   sub     #8, r13\n"
-            "   jc      2b\n"
-            "   subc    r13, r0              ; 0 to 3 cycle delay\n"
+            "   sub     #8, %[cnt]           ;\n"
+            "   jc      2b                   ;\n"
+            "   subc    %[cnt], r0           ; 0 to 3 cycle delay\n"
             "   nop                          ; 3\n"
             "   nop                          ; 2\n"
             "   nop                          ; 1\n"
             "   rra     %[c]                 ; Get bit to tx, test for zero\n"
             "   jc      4f                   ; If high...\n"
-            "3:\n"
-            "   bic.b   r12, %[PXOUT]        ; Send zero bit\n"
+            "3:                              ;\n"
+            "   bic.b   %[mask],%[PXOUT]     ; Send zero bit\n"
             "   jmp     1b                   ; Next bit...\n"
-            "4:\n"
-            "   bis.b   r12, %[PXOUT]        ; Send one bit\n"
+            "4:                              ;\n"
+            "   bis.b   %[mask], %[PXOUT]    ; Send one bit\n"
             "   jnz     1b                   ; If tx data is not zero, then there are more bits to send...\n"
 
             : /* return value */
+            [c] "+r" (c),
+            [cnt] "+r" (cnt)
 
             : /* external variables */
-            [c]           "r" (c),
-            [bit_dur]     "i" (bit_rate.dur),
-            [half_dur]    "i" (bit_rate.dur_half),
-            [tx_bit_mask] "i" (TXPIN::pin_mask),   /* #BIT2  */
-            [PXOUT]       "m" (TXPIN::POUT()) /* &P1OUT */
+            [bit_dur] "r" (bit_dur),
+            [mask] "r" (mask), /* #BIT2  */
+            [PXOUT] "m" (TXPIN::POUT()) /* &P1OUT */
 
             : /* _write() clobbers these registers */
-            "r14", "r13", "r12"
     );
 
     return;
@@ -148,48 +149,44 @@ void serial_base_sw_t<BAUD, MCLK_HZ, TXPIN, RXPIN>::_write(unsigned c)
 /*
  * serial_base_sw_t<>::_read() - read one byte from RX pin
  */
-template <uint32_t BAUD, uint32_t MCLK_HZ, typename TXPIN, typename RXPIN>
-int serial_base_sw_t<BAUD, MCLK_HZ, TXPIN, RXPIN>::_read()
-{
+template<uint32_t BAUD, uint32_t MCLK_HZ, typename TXPIN, typename RXPIN>
+int serial_base_sw_t<BAUD, MCLK_HZ, TXPIN, RXPIN>::_read() {
     register unsigned result;
 
-    __asm__ (
-        " mov     %[bit_dur], r14     ; Bit duration\n"
-        " mov     %[rx_bit_mask], r13 ; Input bitmask\n"
-        " mov     #0x01FF, %[rc]      ; 9 bits - 8 data + stop\n"
+    __asm__ volatile (
+            " mov     %[bit_dur], r14     ; Bit duration\n"
+            " mov     %[rx_bit_mask], r13 ; Input bitmask\n"
+            " mov     #0x01FF, %[rc]      ; 9 bits - 8 data + stop\n"
+            "1:                           ; Wait for start bit\n"
+            " mov.b   %[PXIN], r12        ; Get serial input\n"
+            " and     r13, r12            ; Mask and test bit\n"
+            " jc      1b                  ; Wait for low...\n"
+            " mov     %[half_dur], r13    ; Wait for 1/2 bit time\n"
+            "2:                           ;\n"
+            " nop                         ; Bit delay\n"
+            " sub     #8, r13             ; 1/2 bit adjust\n"
+            " jc      2b\n"
+            " subc    r13, r0             ; 0 to 3 cycle delay\n"
+            " nop                         ; 3\n"
+            " nop                         ; 2\n"
+            " nop                         ; 1\n"
+            " mov.b   %[PXIN], r12        ; Get serial input\n"
+            " and     %[rx_bit_mask], r12 ;\n"
+            " rrc     %[rc]               ; Shift in a bit\n"
+            " mov     r14, r13            ; Setup bit timer\n"
+            " jc      2b                  ; Next bit...\n"
+            "3:                           ; exit\n"
+            " rla     %[rc]               ; Move stop bit to carry\n"
+            " swpb    %[rc]               ; Move rx byte to lower byte, start bit in msb\n"
 
-        "1:                           ; Wait for start bit\n"
-        " mov.b   %[PXIN], r12        ; Get serial input\n"
-        " and     r13, r12            ; Mask and test bit\n"
-        " jc      1b                  ; Wait for low...\n"
-        " mov     %[half_dur], r13    ; Wait for 1/2 bit time\n"
-        "3:\n"
-        " nop                         ; Bit delay\n"
-        " sub     #8, r13             ; 1/2 bit adjust\n"
-        " jc      3b\n"
-        " subc    r13, r0             ; 0 to 3 cycle delay\n"
-        " nop                         ; 3\n"
-        " nop                         ; 2\n"
-        " nop                         ; 1\n"
-        " mov.b   %[PXIN], r12        ; Get serial input\n"
-        " and     %[rx_bit_mask], r12 ;\n"
-        " rrc     %[rc]               ; Shift in a bit\n"
-        " mov     r14, r13            ; Setup bit timer\n"
-        " jc      3b                  ; Next bit...\n"
-
-        " rla     %[rc]               ; Move stop bit to carry\n"
-        " swpb    %[rc]               ; Move rx byte to lower byte, start bit in msb\n"
-
-        "4:\n"
-
-        : /* return value */
+            : /* return value */
             [rc] "=r" (result)
-        : /* external variables */
-            [bit_dur]     "i" (bit_rate.dur),
-            [half_dur]    "i" (bit_rate.dur_half),
-            [rx_bit_mask] "i" (RXPIN::pin_mask),   /* #BIT1, */
-            [PXIN]        "m" (RXPIN::PIN())  /* &P1IN, */
-        : /* _read() clobbers these registers */
+            : /* external variables */
+            [bit_dur] "i" (bit_rate.dur),
+            [half_dur] "i" (bit_rate.dur_half),
+            [rx_bit_mask] "i" (RXPIN::pin_mask), /* #BIT1 */
+            [PXIN] "m" (RXPIN::PIN()) /* &P1IN */
+            : /* _read() clobbers these registers */
             "r14", "r13", "r12"
     );
 
